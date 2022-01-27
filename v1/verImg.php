98f7e9aacd4ed4bd05f349d39841c8fa138ec169
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2022-01-27 10:10:04 +0100

    v1 visualizar img incompleta

---
+++ v1/verImg.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once "metodos.php";
+    $metodos = new Metodos();
+?>
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Subir Archivos Servidor V1</title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">Subir Imágenes</a></li>
+                <li><a href="">Ver Nombre de la Imagen</a></li>
+                <li><a href="">Ver Imágenes</a></li>
+            </ul>
+        </nav>
+
+        <h2>Archivos</h2>
+
+        <?php
+
+            $metodos->
+        ?>
+    </body>
+</html>
